0b90c0ec6d5a3b3a118696874f6b4cdb24ecbdec
cocojack <constantin@jaecktec.de>
2017-01-08 16:03:34 +0100

    removed value service, because it has nothing todo with a registry. storiung values is more of a job for another project.

--- src/services/value_service/ValueService.js
+++ src/services/value_service/ValueService.js
@@ -24,8 +24,6 @@ class ValueService {
             var channel = yield connection.createConfirmChannel();
             channel.prefetch(1);
             channel.assertQueue(ValueServiceQueue.mainQueue, {exclusive: false, durable: true});
-            channel.assertQueue(ValueServiceQueue.newValueQueue, {exclusive: false, durable: true});
-            channel.assertQueue(ValueServiceQueue.valueRpcQueue, {exclusive: false, durable: true});
 
             yield AmqpExchanges.createExchanges(channel);
 
@@ -36,8 +34,6 @@ class ValueService {
             ];
 
             channel.consume(ValueServiceQueue.mainQueue, (msg)=> AmqpHelper.handleAck(msg, channel, _this.__onValueMessage), {noAck: false});
-            channel.consume(ValueServiceQueue.newValueQueue, (msg)=> AmqpHelper.handleAck(msg, channel, _this.__createNewValue), {noAck: false});
-            channel.consume(ValueServiceQueue.valueRpcQueue, (msg)=> AmqpHelper.handleRpcRquest(msg, channel, _this.__handleGet), {noAck: false});
 
             // MONGODB connect
             if (!mongoose.connection.readyState) {
@@ -69,56 +65,6 @@ class ValueService {
         })(this, msg, channel);
     }
 
-    /**
-     * Saves Value
-     * @param msg amqp message
-     * @returns {*} promise
-     * @private
-     */
-    __createNewValue(msg, channel) {
-        return co.wrap(function*(_this, msg, channel) {
-            debug("create new value", msg);
-            let value = yield new DbValue({
-                nodeId: msg.nodeId,
-                deviceId: msg.deviceId,
-                value: msg.message,
-                created: new Date()
-            }).save();
-
-            // disabled because cant test it
-            // let device = (yield AmqpHelper.rpcRequest({
-            //     nodeId: msg.nodeId,
-            //     id: msg.deviceId
-            // }, AmqpExchanges.DEVICE_API_EXCHANGE, DeviceServiceRoutingKey.ROUTING_KEY_RPC_GET_DEVICE, channel))[0];
-            // let valuesCount = yield DbValue.count({
-            //     nodeId: msg.nodeId,
-            //     deviceId: msg.deviceId
-            // });
-            // if (device.store && device.store.maxCount && device.store.maxCount < valuesCount) {
-            //     let all = yield DbValue.find().sort({_id: -1});
-            //     let removeValues = all.slice(device.store.maxCount, all.length);
-            //     yield Promise.all(removeValues.map(value => value.remove()));
-            // }
-
-            channel.publish(
-                AmqpExchanges.VALUE_API_EXCHANGE,
-                ValueServiceRoutingKey.ROUTING_KEY_VALUE_NEW_STORED,
-                AmqpHelper.objToBuffer(value));
-            return yield Promise.resolve();
-        })(this, msg, channel);
-    }
-
-    __handleGet(request) {
-        let nodeId = request.nodeId;
-        let deviceId = request.deviceId;
-        return co.wrap(function*() {
-            let values = yield DbValue.find({
-                nodeId: nodeId,
-                deviceId: deviceId
-            });
-            return yield Promise.resolve(values);
-        })();
-    }
 }
 
 module.exports = new ValueService();
--- src/services/value_service/constants/ValueServiceQueue.js
+++ src/services/value_service/constants/ValueServiceQueue.js
@@ -3,13 +3,5 @@ class ValueServiceQueue {
         return "dr.value";
     }
 
-    static get newValueQueue() {
-        return "dr.value.new";
-    }
-
-    static get valueRpcQueue() {
-        return "dr.value.rpc"
-    }
-
 }
 module.exports = ValueServiceQueue;
